be1be38955220661114bf650a34413162cd1b733
Arka Banerjee <arkabanerjee381@gmail.com>
2026-06-26 19:34:42 +0530

    back mfs

--- calculate-gcd.py
+++ calculate-gcd.py
@@ -1,3 +1,13 @@
-a = 12
-b = 18
+a = int(input())
+b = int(input())
 
+factors = []
+
+for i in range (1, a+1):
+    for j in range (1, b+1):
+        if (a % i == 0):
+            if (b % j == 0):
+                if (i == j):
+                    factors.append(i)
+
+print (factors[-1])
